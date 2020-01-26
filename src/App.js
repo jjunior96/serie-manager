12056185a93ac8de19de664fae626038c3e6fe73
Junior Alves <jjalves96@gmail.com>
2020-01-25 22:14:37 -0300

    feat: using Axios, but bug in the port 3002/api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header'
@@ -16,6 +17,14 @@ const Genres = () => {
 }
 
 function App() {  
+  const [ data, setData ] = useState({});
+
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+  }, []);
+
   return (
     <Router >
       <Header />
@@ -24,6 +33,7 @@ function App() {
         {/* Exact = retorna o componente onde é exatamente esse path */}
         <Route path='/' exact component={Home} />
         <Route path='/genres' component={Genres} />
+        <pre>{JSON.stringify(data)}</pre>
       </Switch>
     </Router>
   );
