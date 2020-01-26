e46ee2155b84e5bafed4b3b41762611be9edd7e9
Junior Alves <jjalves96@gmail.com>
2020-01-25 21:20:49 -0300

    feat: Navbar create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { 
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} from 'reactstrap';
 
 function App() {
+  const [ open, setOpen ] = useState(false);
+
+  const handleToggle = () => {
+    setOpen(!open);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar color='light' light expand='md' >
+        <NavbarBrand>Minhas series</NavbarBrand>
+        <NavbarToggler onClick={handleToggle}/>
+        <Collapse isOpen={open} navbar >
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Generos</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
 }
